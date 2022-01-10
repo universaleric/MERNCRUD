61c08103077bb92f491a0e084e8e3c72e1e0d804
Eric Hay <ericdanielhay@yahoo.com>
2022-01-10 14:56:05 -0600

    Added an update and delete button to home page.

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../components/Nav";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
@@ -32,15 +32,29 @@ const App = () => {
           style={{ borderBottom: "1px solid silver" }}
         >
           <div className="col pt-3 pb-2">
-            <Link to={`/post/${post.slug}`}><h2>{post.title}</h2></Link>
-            <p className="lead">{post.content.substring(0, 100)}</p>
-            <p>
-              Author <span className="text-muted">{post.user}</span> Published
-              on{" "}
-              <span className="text-muted">
-                {new Date(post.createdAt).toLocaleString()}
-              </span>
-            </p>
+            <div className="row">
+              <div className="col-md-10">
+                <Link to={`/post/${post.slug}`}>
+                  <h2>{post.title}</h2>
+                </Link>
+                <p className="lead">{post.content.substring(0, 100)}</p>
+                <p>
+                  Author <span className="text-muted">{post.user}</span>{" "}
+                  Published on{" "}
+                  <span className="text-muted">
+                    {new Date(post.createdAt).toLocaleString()}
+                  </span>
+                </p>
+              </div>
+              <div className="col-md-2">
+                <Link to={`/post/update/${post.slug}`} className="btn btn-sm btn-outline-warning">
+                  Update
+                </Link>
+                <button className="btn btn-sm btn-outline-danger m-1">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       ))}
